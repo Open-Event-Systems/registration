5c36914c029b0df8e7675d1933c66f6d1b5b6661
Wes Weber <wweber@wesweber.com>
2023-12-25 15:02:09 -0500

    Add queue entity creation function

--- python/registration/src/oes/registration/queue/functions.py
+++ python/registration/src/oes/registration/queue/functions.py
@@ -1,13 +1,77 @@
 """Queue functions."""
 import re
+import uuid
 from typing import Optional, cast
 
 from oes.registration.checkout.service import CheckoutService
+from oes.registration.entities.registration import RegistrationEntity
 from oes.registration.models.event import QueueConfig
 from oes.registration.models.registration import Registration
+from oes.registration.queue.entities import QueueItemEntity
 from oes.registration.queue.models import QueueItemData
 from oes.registration.serialization import get_converter
+from oes.registration.services.registration import RegistrationService
 from oes.template import evaluate
+
+
+async def make_queue_item(
+    scan_data: Optional[str] = None,
+    /,
+    *,
+    config: QueueConfig,
+    checkout_service: CheckoutService,
+    registration_service: RegistrationService,
+) -> QueueItemEntity:
+    """Make a :class:`QueueItemEntity` from scan data.
+
+    Args:
+        scan_data: The scanned data.
+        config: The checkout config.
+        checkout_service: The checkout service.
+        registration_service: The registration service.
+    """
+    reg = await _parse_scan_data(scan_data, checkout_service, registration_service)
+
+    # TODO: ID parsing
+
+    if reg:
+        item_data = get_queue_item_data(
+            reg.get_model(), scan_data=scan_data, config=config
+        )
+    else:
+        item_data = QueueItemData(scan_data=scan_data)
+
+    entity = QueueItemEntity(
+        id=uuid.uuid4(),
+    )
+    entity.set_data(item_data)
+    return entity
+
+
+async def _parse_scan_data(
+    scan_data: Optional[str],
+    checkout_service: CheckoutService,
+    registration_service: RegistrationService,
+) -> Optional[RegistrationEntity]:
+    if scan_data and (scan_data.startswith("http:") or scan_data.startswith("https:")):
+        receipt_id, index = _parse_receipt_url(scan_data)
+        return await _get_reg_by_receipt_id(
+            receipt_id, index, checkout_service, registration_service
+        )
+    return None
+
+
+async def _get_reg_by_receipt_id(
+    receipt_id: str,
+    index: int,
+    checkout_service: CheckoutService,
+    registration_service: RegistrationService,
+) -> Optional[RegistrationEntity]:
+    if receipt_id:
+        reg_id = await _get_reg_id_by_receipt(receipt_id, index, checkout_service)
+        if reg_id:
+            return await registration_service.get_registration(reg_id)
+    return None
 
 
 def get_queue_item_data(
