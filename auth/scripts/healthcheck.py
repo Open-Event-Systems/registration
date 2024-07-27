fc403f185a4bada8a73716546704a252a3104939
Wes <wes@anthronewengland.com>
2024-07-27 15:43:21 -0400

    Update health check

--- auth/scripts/healthcheck.py
+++ auth/scripts/healthcheck.py
@@ -3,6 +3,7 @@
 from datetime import datetime, timedelta
 
 import httpx
+from oes.auth.auth import Scope
 from oes.auth.config import get_config
 from oes.auth.token import AccessToken
 
@@ -17,11 +18,13 @@ token = AccessToken(
     exp=datetime.now().astimezone() + timedelta(seconds=5),
     sub="<healthcheck>",
     email="test@example.net",
+    scope=frozenset((Scope.selfservice,)),
 )
 auth_str = f"Bearer {token.encode(key=config.token_secret)}"
 
 res = httpx.get(
-    "http://localhost:8000/auth/validate", headers={"Authorization": auth_str}
+    "http://localhost:8000/auth/validate",
+    headers={"Authorization": auth_str, "x-original-uri": "/self-service/events"},
 )
 res.raise_for_status()
 email = res.headers.get("x-email")
